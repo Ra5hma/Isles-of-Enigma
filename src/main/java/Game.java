27d9772805fff84067b0b5102c09db9d4f98fb36
Scott Underwood <atraydes@gmail.com>
2022-01-31 20:17:39 -0600

    worked, but shows memory location instead of real output

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,11 +1,12 @@
-package Actor;
-import java.util.Scanner;
+import Actor.Actor;
 
 public class Game {
-    public static void main(String[] args) {
-        System.out.println("hello world");
 
-        Actor actor1 = new Actor("LazerFace", 22);
-        System.out.println(actor1);
-    }
+
+        public static void main(String[] args) {
+            System.out.println("hello world");
+
+            Actor actor1 = new Actor("LazerFace", 22);
+            System.out.println(actor1.toString());
+        }
 }
